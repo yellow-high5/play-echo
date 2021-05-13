10ed8d3e7d832e27a77da1dd9852bc948b016912
yoshinori.matsuzaki <yoshinori.matsuzaki@monocrea.co.jp>
2021-05-13 21:57:18 +0900

    fix inser_data sql

--- db/migrations/2_insert_data.sql
+++ db/migrations/2_insert_data.sql
@@ -4,4 +4,4 @@ insert into users values(1, "2021-05-01 12:45:35", "2021-05-01 12:45:35", null, 
 insert into hosts values(1, "2021-05-01 11:45:35", "2021-05-01 11:45:35", null, "host1", "tanaka", "", "", "012000001111", "tokyo", "", true, false);
 
 insert into plans values(1, "2021-05-01 11:45:35", "2021-05-01 11:45:35", null, 1, "planA", "description: planA", 8000, "share", "", 24, "", "", "", null);
-insert into plans values(2, "2021-05-01 11:45:35", "2021-05-01 11:45:35", null, 2, "planB", "description: planB", 12000, "share", "", 12, "", "", "", null);
+insert into plans values(2, "2021-05-01 11:45:35", "2021-05-01 11:45:35", null, 1, "planB", "description: planB", 12000, "share", "", 12, "", "", "", null);
